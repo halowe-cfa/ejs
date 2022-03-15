75a6f340debac5116bbb041f96d2a9646db2960b
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-03-15 14:54:22 -0700

    Update app.js
    added additional instructions

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
+//run npm init
+//npm i express
+//in your package.json, change "main": "index.js" to "main": "app.js"
 //create a basic server using const express, app.get, and app.listen
 //use res.send to send "Welcome"
